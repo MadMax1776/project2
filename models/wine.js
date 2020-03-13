7c83d2fee15db86c652907762c9ec3846a843060
mmeacham <maxwmeacham@gmail.com>
2020-03-13 17:57:52 -0400

    created schema and index and new on crud

--- models/wine.js
+++ models/wine.js
@@ -1,31 +1,31 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+const wineSchema = new mongoose.Schema({
+    name: { type: String, required:true },
+    color: { type: String, required:true },
+    country: { type: String, required:true }
+});
+
+const Wine = mongoose.model('Wine', wineSchema);
+
+module.exports = Wine;
+
+// const wines = [
+//     {
+//         name:'Suavignon',
+//         color: 'red',
+//         country: 'France'
+//     },
+//     {
+//         name:'Casillero Del Diablo',
+//         color: 'red',
+//         country: 'Chile'
+//     },
+//     {
+//         name:'Khvanchkara',
+//         color: 'red',
+//         country: 'Republic of Georgia'
+//     }
+// ];
 //
-// const wineSchema = new mongoose.Schema({
-//     name: { type: String, required:true },
-//     color: { type: String, required:true },
-//     country: { type: String, required:true }
-// });
-//
-// const Wine = mongoose.model('Wine', wineSchema);
-
-// module.exports = Wine;
-
-const wines = [
-    {
-        name:'Suavignon',
-        color: 'red',
-        country: 'France'
-    },
-    {
-        name:'Casillero Del Diablo',
-        color: 'red',
-        country: 'Chile'
-    },
-    {
-        name:'Khvanchkara',
-        color: 'red',
-        country: 'Republic of Georgia'
-    }
-];
-
-module.exports = wines;
+// module.exports = wines;
--- server.js
+++ server.js
@@ -22,70 +22,86 @@ db.on('open', () => {
     console.log('Connection made!');
 });
 //////==========================================================
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 ////===================================================
-
-const wines = require('./models/wine.js');
-
-app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
-app.use((req, res, next) => {
-  // console.log('I run for all routes');
-  next();
-})
-
-
+////================CRUD========================================
+const Wine = require('./models/wine.js');
 
 app.get('/wines/new', (req, res) => {
   res.render('new.ejs');
 });
 
-
-app.get('/wines/', (req, res) => {
-    res.render('index.ejs',
-  {
-    allWines: wines
+app.post('/wines/', (req, res)=>{
+  Wine.create(req.body, (error, createdWine) => {
+      res.send(createdWine);
   });
 });
 
-app.get('/wines/:indexOfWinesArray', (req, res) => {
-  res.render('show.ejs', {
-    wines: wines[req.params.indexOfWinesArray]
+app.get('/wines', (req, res) => {
+  Wine.find({}, (error, allWines) => {
+    res.render('index.ejs', {
+      wines: allWines
+    });
   });
 });
 
-app.post('/wines', (req, res) => {
-  wines.push(req.body)
-  res.redirect('/wines'); //sends user back to /wines
-});
+/////============================================================
+//
+//
+// app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+// app.use((req, res, next) => {
+//   // console.log('I run for all routes');
+//   next();
+// })
+//
+// app.get('/wines/new', (req, res) => {
+//   res.render('new.ejs');
+// });
+//
+// app.get('/wines/', (req, res) => {
+//     res.render('index.ejs',
+//   {
+//     allWines: wines
+//   });
+// });
+//
+// app.get('/wines/:indexOfWinesArray', (req, res) => {
+//   res.render('show.ejs', {
+//     wines: wines[req.params.indexOfWinesArray]
+//   });
+// });
+//
+// app.post('/wines', (req, res) => {
+//   wines.push(req.body)
+//   res.redirect('/wines'); //sends user back to /wines
+// });
+//
+// app.delete('/wines/:indexOfWinesArray', (req, res) => {
+//   wines.splice(req.params.indexOfWinesArray, 1); //remove item from the array
+//   res.redirect('/wines'); //redirect back to the index route
+// })
+//
+// app.get('/wines/:indexOfWinesArray/edit', (req, res) => {
+//   res.render(
+//     'edit.ejs', // render views/edit.ejs
+//     { //pass in an object that contains
+//       wines: wines[req.params.indexOfWinesArray], //the wines object
+//       index: req.params.indexOfWinesArray //the wine index in the array
+//     }
+//   );
+// })
+//
+// app.put('/wines/:indexOfWinesArray', (req, res) => {
+//   wines[req.params.indexOfWinesArray] = req.body; // find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+//   res.redirect('/wines');
+// });
 
-app.delete('/wines/:indexOfWinesArray', (req, res) => {
-  wines.splice(req.params.indexOfWinesArray, 1); //remove item from the array
-  res.redirect('/wines'); //redirect back to the index route
-})
 
-app.get('/wines/:indexOfWinesArray/edit', (req, res) => {
-  res.render(
-    'edit.ejs', // render views/edit.ejs
-    { //pass in an object that contains
-      wines: wines[req.params.indexOfWinesArray], //the wines object
-      index: req.params.indexOfWinesArray //the wine index in the array
-    }
-  );
-})
-
-app.put('/wines/:indexOfWinesArray', (req, res) => {
-  wines[req.params.indexOfWinesArray] = req.body; // find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
-  res.redirect('/wines');
-});
-
-
-
-
-////==================================================
-app.get('/', (req, res) => {
-  res.send('your application is working');
-});
+////================================================================
+// app.get('/', (req, res) => {
+//   res.send('your application is working');
+// });
 
 
 
